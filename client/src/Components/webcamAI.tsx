38d95c72a56bf70e770363445f46c7c7115f637f
Natasha Vermeulen <natashavermeulen@Hendriks-MacBook-Pro.local>
2022-02-01 10:24:45 +0100

    feat(counts): uses primitive variable to count
    Will need useState from parent to trigger next step (and make count visible to client)

--- client/src/Components/webcamAI.tsx
+++ client/src/Components/webcamAI.tsx
@@ -1,26 +1,29 @@
-import Webcam from 'react-webcam';
-import * as tmPose from '@teachablemachine/pose';
-import { useRef, useState } from 'react';
+import Webcam from "react-webcam";
+import * as tmPose from "@teachablemachine/pose";
+import { useRef, useState } from "react";
 
 const URL = "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/";
 // const URL = 'https://teachablemachine.withgoogle.com/models/jwj-LGant/';
-let model: {getTotalClasses: Function, estimatePose: Function, predict: Function}, ctx: CanvasRenderingContext2D, maxPredictions: number;
+let model: { getTotalClasses: Function; estimatePose: Function; predict: Function },
+  ctx: CanvasRenderingContext2D,
+  maxPredictions: number;
 
+const WebcamAI: React.FC = () => {
+  let repCount = 0; //will need to be useState passed from parent so that it's visible and triggers next
+  let repStatus: string = "Neutral";
 
-const WebcamAI = () => {
-  const [size, setSize] = useState(window.innerWidth * 0.9)
-  window.onresize = () => {
+  const [size, setSize] = useState<number>(window.innerWidth * 0.9);
+  window.onresize = (): void => {
     setSize(window.innerWidth * 0.9);
-  }
+  };
 
   const webcamRef = useRef<Webcam>(null);
 
-  async function init() {
+  async function init(): Promise<void> {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
     model = await tmPose.load(modelURL, metadataURL);
-    console.log('model', model)
     maxPredictions = model.getTotalClasses();
     window.requestAnimationFrame(loop);
 
@@ -45,37 +48,35 @@ const WebcamAI = () => {
     const canvas: HTMLCanvasElement = getCanvasElementById("canvas");
 
     if (canvas) {
-      console.log(canvas);
       canvas.width = size;
       canvas.height = size * 0.75;
       ctx = getCanvasRenderingContext2D(canvas)!;
-
     }
   }
 
-  async function loop(timestamp: number) {
+  async function loop(): Promise<void> {
     await predict();
     window.requestAnimationFrame(loop);
   }
 
-  async function predict() {
+  async function predict(): Promise<void> {
     if (webcamRef.current !== null) {
-      const { pose, posenetOutput } = await model.estimatePose(
-        webcamRef.current.getCanvas()
-      );
+      const { pose, posenetOutput } = await model.estimatePose(webcamRef.current.getCanvas());
       const prediction = await model.predict(posenetOutput);
-
       for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction = prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-          // labelContainer.childNodes[i].textContent = classPrediction;
-          // use classPrediction to count reps & change page
+        if (prediction[i].probability.toFixed(2) > 0.95) {
+          if (prediction[i].className !== repStatus && prediction[i].className !== "Neutral") {
+            repStatus = prediction[i].className;
+            repCount++;
+            console.log(repCount);
+          }
+        }
       }
       drawPose(pose);
     }
   }
 
-  function drawPose(pose: any):void {
-    console.log(webcamRef);
+  function drawPose(pose: any): void {
     if (webcamRef.current !== null) {
       if (webcamRef.current.getCanvas()) {
         ctx.drawImage(webcamRef.current.getCanvas()!, 0, 0);
@@ -95,39 +96,38 @@ const WebcamAI = () => {
         <Webcam
           ref={webcamRef}
           style={{
-            position: 'absolute',
-            marginLeft: 'auto',
-            marginRight: 'auto',
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
             right: 0,
             left: 0,
             zIndex: 9,
             width: size,
             height: size * 0.75,
-            borderRadius: 2,
+            borderRadius: 2
           }}
           onUserMedia={init}
           mirrored={true}
         />
-      ): <div>
-        Teachable Machine Pose Model
-        </div>}
+      ) : (
+        <div>Teachable Machine Pose Model</div>
+      )}
 
-        <canvas
-          id='canvas'
-          style={{
-            position: 'absolute',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            left: 0,
-            right: 0,
-            textAlign: 'center',
-            zIndex: 10,
-            width: size,
-            height: size * 0.75,
-          }}
-        ></canvas>
+      <canvas
+        id="canvas"
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 10,
+          width: size,
+          height: size * 0.75
+        }}></canvas>
     </>
   );
-}
+};
 
 export default WebcamAI;
