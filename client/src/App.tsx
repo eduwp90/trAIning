d5b4542ca8881f0c659080923491c50f61fefc8f
Natasha Vermeulen <natashavermeulen@Hendriks-MacBook-Pro.local>
2022-02-02 14:08:03 +0100

    feat(working less): customize Ant with Less and Craco

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import WorkoutForm from './Pages/workoutForm';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import Home from './Pages/home';
-import Main from './Pages/main';
-import Login from './Pages/login';
-import Register from './Pages/register';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import AuthService from './Services/authService';
+import React, { useEffect } from "react";
+import "./App.less";
+import WorkoutForm from "./Pages/workoutForm";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Pages/home";
+import Main from "./Pages/main";
+import Login from "./Pages/login";
+import Register from "./Pages/register";
+import { useAuthState } from "react-firebase-hooks/auth";
+import AuthService from "./Services/authService";
 
 const App: React.FC = () => {
   const [user, loading] = useAuthState(AuthService.auth);
@@ -18,19 +18,19 @@ const App: React.FC = () => {
       // maybe trigger a loading screen
       return;
     }
-    if (!user) navigate('/login');
+    if (!user) navigate("/login");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, loading]);
 
   return (
     <Routes>
-      <Route path='/' element={<Main />}>
+      <Route path="/" element={<Main />}>
         <Route index element={<Home />} />
-        <Route path='home' element={<Home />} />
-        <Route path='workout' element={<WorkoutForm />} />
+        <Route path="home" element={<Home />} />
+        <Route path="workout" element={<WorkoutForm />} />
       </Route>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
