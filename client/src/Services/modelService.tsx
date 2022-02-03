e219daa55c3a63d1093b6a9644a20fde3c0c3867
Natasha Vermeulen <natashavermeulen@Hendriks-MacBook-Pro.local>
2022-02-03 10:35:03 +0100

    feat(modelService): service to provide model url by type of workout

---
+++ client/src/Services/modelService.tsx
@@ -0,0 +1,10 @@
+const modelsByType = {
+  squats: "url",
+  lunges: "url",
+  "jumping-jacks": "url",
+  "side-squats": "url",
+  pushups: "url",
+  arms: "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/"
+};
+
+export default modelsByType;
