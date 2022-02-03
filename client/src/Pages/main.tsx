037dafbee56ed7c218fd9b366dd3b1c09b3a8099
eduwp90 <eduwp90@gmail.com>
2022-02-03 16:33:41 +0100

    navbar with user, menu & logout

--- client/src/Pages/main.tsx
+++ client/src/Pages/main.tsx
@@ -1,28 +1,58 @@
 import React from "react";
-import { Avatar, Layout, Menu } from "antd";
+import { Avatar, Layout, Menu, Image } from "antd";
 import "./pages.less";
-import { Outlet } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
+import { Link, Outlet } from "react-router-dom";
+import { LogoutOutlined } from "@ant-design/icons";
+import { useAuthState } from "react-firebase-hooks/auth";
+import AuthService from "../Services/authService";
 
 const { Header, Content } = Layout;
 
 const Main: React.FC = () => {
+  const [user] = useAuthState(AuthService.auth);
+
+  const logout = (): void => {
+    AuthService.logoutUser();
+  };
+
+  console.log("user ", user);
   return (
-    <Layout>
-      <Header>
+    <Layout className="layout">
+      <Header className="navbar">
         <div className="nav-logo">LOGO</div>
         <div className="nav-content">
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["0"]}>
-            <Menu.Item key={0}>{`Home`}</Menu.Item>
+            <Menu.Item key={0}>
+              <Link to="/home">{`Home`}</Link>
+            </Menu.Item>
           </Menu>
         </div>
         <div className="nav-user">
-          <Avatar
-            style={{
-              backgroundColor: "#87d068"
-            }}
-            icon={<UserOutlined />}
-          />
+          {user && (
+            <Menu mode="horizontal" className="user-menu" theme="dark">
+              <Menu.SubMenu
+                key={"submenu"}
+                title={
+                  <Avatar
+                    src={
+                      user?.photoURL && (
+                        <Image
+                          src={user.photoURL}
+                          style={{
+                            width: 32
+                          }}
+                        />
+                      )
+                    }>
+                    {!user.photoURL && `${user.email?.charAt(0).toUpperCase()}`}
+                  </Avatar>
+                }>
+                <Menu.Item key="logout" onClick={logout}>
+                  <LogoutOutlined /> Logout
+                </Menu.Item>
+              </Menu.SubMenu>
+            </Menu>
+          )}
         </div>
       </Header>
       <Content>
