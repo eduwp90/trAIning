71d2416ff4a27ee08997b678ce69657df07c16ee
Natasha Vermeulen <natashavermeulen@Hendriks-MacBook-Pro.local>
2022-02-03 12:02:15 +0100

    feat(modelService): rendering URLs from modelService - still just one URL for all models

--- client/src/Services/modelService.tsx
+++ client/src/Services/modelService.tsx
@@ -1,9 +1,13 @@
-const modelsByType = {
-  squats: "url",
-  lunges: "url",
-  "jumping-jacks": "url",
-  "side-squats": "url",
-  pushups: "url",
+type tmodelsByType = {
+  [key: string]: string;
+};
+
+const modelsByType: tmodelsByType = {
+  squats: "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/",
+  lunges: "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/",
+  "jumping-jacks": "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/",
+  "side-squats": "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/",
+  pushups: "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/",
   arms: "https://teachablemachine.withgoogle.com/models/HQvC3rR8v/"
 };
 
