c5369075651053cb7a4a1f7476022b59c0297d00
eduwp90 <eduwp90@gmail.com>
2022-02-03 18:45:39 +0100

    fix: improved TS on dbservice

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -1,11 +1,17 @@
+import { DocumentData } from "firebase/firestore/lite";
+
 export interface ISet {
   exer: string;
   reps: number;
   rest: number;
 }
 
-export interface IWorkout{
+export interface IWorkout {
   user: string;
   name: string;
   workout: ISet[];
+}
+
+export interface IWorkoutResponse extends IWorkout, DocumentData {
+  id: string;
 }
