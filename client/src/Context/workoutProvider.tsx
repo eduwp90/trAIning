cc8734fd2364aa52f429626597408be7e957efbd
Natasha Vermeulen <natashavermeulen@Hendriks-MacBook-Pro.local>
2022-02-04 14:37:48 +0100

    feat(working)

--- client/src/Context/workoutProvider.tsx
+++ client/src/Context/workoutProvider.tsx
@@ -1,26 +1,37 @@
-import React, { createContext, useState } from 'react';
-import { ISet, IWorkoutContext } from '../interfaces';
+import React, { createContext, useState, useEffect, Dispatch, SetStateAction } from "react";
+import { ISet, IWorkoutContext } from "../interfaces";
 
-const contextDefaultValues: IWorkoutContext = { workout: [], storeWorkout: () => { }, clearWorkout: () => { } };
+const contextDefaultValues: IWorkoutContext = { workout: [], storeWorkout: () => {}, clearWorkout: () => {} };
 
-export const WorkoutContext = createContext<IWorkoutContext >(contextDefaultValues)
+export const WorkoutContext = createContext<IWorkoutContext>(contextDefaultValues);
 
 const WorkoutProvider: React.FC = ({ children }) => {
-  const [workout, setWorkout] = useState<ISet[]>([]);
+  const [workout, setWorkout] = useStateWithLocalStorageForArray("storedWorkout");
+  //useState<ISet[]>([]);
 
-  const storeWorkout = (sets: ISet[]):void=> {
-  setWorkout(sets)
-  }
+  const storeWorkout = (sets: ISet[]): void => {
+    setWorkout(JSON.stringify(sets));
+  };
 
   const clearWorkout = (): void => {
-  setWorkout([])
-  }
+    setWorkout([]);
+  };
 
-  return (
-    <WorkoutContext.Provider value={{workout, storeWorkout, clearWorkout}}>
-    {children}
-    </WorkoutContext.Provider>
-  );
-}
+  return <WorkoutContext.Provider value={{ workout, storeWorkout, clearWorkout }}>{children}</WorkoutContext.Provider>;
+};
 
 export default WorkoutProvider;
+
+const useStateWithLocalStorageForArray = (localStorageKey: string): [string, Dispatch<SetStateAction<string>>] => {
+  let savedArray = localStorage.getItem(localStorageKey) || [];
+  if (typeof savedArray === 'string')
+    savedArray = JSON.parse(savedArray);
+  } 
+  const [value, setValue] = useState(savedArray);
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(value));
+  }, [value, localStorageKey]);
+
+  return [value, setValue];
+};
--- client/src/Pages/workoutSummary.tsx
+++ client/src/Pages/workoutSummary.tsx
@@ -1,126 +1,146 @@
-import { CloseOutlined } from '@ant-design/icons';
-import { Button, Form, Input, InputNumber, Radio, Select  } from 'antd';
-import React, { useContext, useEffect, useState } from 'react';
-import { ISet, IWorkoutContext } from '../interfaces';
-import "./pages.less"
-import "../Components/components.less"
-import { addWorkout } from '../Services/dbService';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import AuthService from '../Services/authService';
-import { WorkoutContext } from '../Context/workoutProvider';
-import { useNavigate } from 'react-router-dom';
+import { CloseOutlined } from "@ant-design/icons";
+import { Button, Form, Input, InputNumber, Radio, Select } from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { ISet, IWorkoutContext } from "../interfaces";
+import "./pages.less";
+import "../Components/components.less";
+import { addWorkout } from "../Services/dbService";
+import { useAuthState } from "react-firebase-hooks/auth";
+import AuthService from "../Services/authService";
+import { WorkoutContext } from "../Context/workoutProvider";
+import { useNavigate } from "react-router-dom";
 const { Option } = Select;
-
-
 
 const WorkoutSummary: React.FC = () => {
   const [user] = useAuthState(AuthService.auth);
   const [setsDisabled, setSetsDisabled] = useState<boolean>(true);
   const [sets, setSets] = useState<JSX.Element[]>([]);
-  const {workout, clearWorkout} = useContext<IWorkoutContext>(WorkoutContext)
-  const navigate = useNavigate()
+  const { workout, clearWorkout } = useContext<IWorkoutContext>(WorkoutContext);
+  const navigate = useNavigate();
 
   const onFinish = (e: React.FormEvent<HTMLInputElement>): void => {
-    const workoutArray = Object.values(e)
-    const name: string = workoutArray.pop()
-    const sets: ISet[] = workoutArray
+    const workoutArray = Object.values(e);
+    const name: string = workoutArray.pop();
+    const sets: ISet[] = workoutArray;
     if (user) {
       addWorkout(user.uid, sets, name)
-        .then(() => { clearWorkout() })
-        .then(() => { navigate('/') })
-        .catch((e)=>{console.log(e)})
+        .then(() => {
+          clearWorkout();
+        })
+        .then(() => {
+          navigate("/");
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
-   };
+  };
 
-  const setsArray: JSX.Element[] = workout.map(set => {
-    const id: number = workout.indexOf(set)
-    return (<div key={id} id={`${id}`} className="set-Div">
-      <div className="set-Div_inputs">
-        <Form.Item
-          name={[id, "exer"]}
-          label="Select Exercise"
-          rules={[
-            {
-              required: true
-            }
-          ]}
-        initialValue={set.exer}>
-          <Select size="large" placeholder="exercise" style={{ width: 120 }} disabled={setsDisabled}>
-            <Option value="push-ups">push ups</Option>
-            <Option value="squats">squats</Option>
-            <Option value="lunges">lunges</Option>
-            <Option value="jumping-jacks">jumping jacks</Option>
-            <Option value="side-squats">side squats</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name={[id,  "reps"]}
-          label="Nº of repetitions"
-          rules={[
-            {
-              required: true
-            }
-          ]}
-        initialValue={set.reps}>
-          <InputNumber size="large" placeholder="Reps" min={1} max={30} style={{ width: 120 }} disabled={setsDisabled} />
-        </Form.Item>
-
-        <Form.Item
-          name={[id, "rest"]}
-          label="Rest time"
-          rules={[
-            {
-              required: true
-            }
-          ]}
-        initialValue={set.rest}>
-          <Radio.Group size="large" disabled={setsDisabled} buttonStyle="solid">
-            <Radio.Button value={0}>0 min</Radio.Button>
-            <Radio.Button value={1}>1 min</Radio.Button>
-            <Radio.Button value={3}>3 min</Radio.Button>
-            <Radio.Button value={5}>5 min</Radio.Button>
-          </Radio.Group>
+  const setsArray: JSX.Element[] = workout.map((set) => {
+    const id: number = workout.indexOf(set);
+    return (
+      <div key={id} id={`${id}`} className="set-Div">
+        <div className="set-Div_inputs">
+          <Form.Item
+            name={[id, "exer"]}
+            label="Select Exercise"
+            rules={[
+              {
+                required: true
+              }
+            ]}
+            initialValue={set.exer}>
+            <Select size="large" placeholder="exercise" style={{ width: 120 }} disabled={setsDisabled}>
+              <Option value="push-ups">push ups</Option>
+              <Option value="squats">squats</Option>
+              <Option value="lunges">lunges</Option>
+              <Option value="jumping-jacks">jumping jacks</Option>
+              <Option value="side-squats">side squats</Option>
+            </Select>
           </Form.Item>
+          <Form.Item
+            name={[id, "reps"]}
+            label="Nº of repetitions"
+            rules={[
+              {
+                required: true
+              }
+            ]}
+            initialValue={set.reps}>
+            <InputNumber
+              size="large"
+              placeholder="Reps"
+              min={1}
+              max={30}
+              style={{ width: 120 }}
+              disabled={setsDisabled}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name={[id, "rest"]}
+            label="Rest time"
+            rules={[
+              {
+                required: true
+              }
+            ]}
+            initialValue={set.rest}>
+            <Radio.Group size="large" disabled={setsDisabled} buttonStyle="solid">
+              <Radio.Button value={0}>0 min</Radio.Button>
+              <Radio.Button value={1}>1 min</Radio.Button>
+              <Radio.Button value={3}>3 min</Radio.Button>
+              <Radio.Button value={5}>5 min</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+        </div>
+        <Button className="round_button" onClick={() => removeSet(id)}>
+          <CloseOutlined />
+        </Button>
       </div>
-      <Button className="round_button" onClick={() => removeSet(id) }>
-        <CloseOutlined />
-      </Button>
-    </div>)
-  })
+    );
+  });
 
   function removeSet(id: number): void {
     setSets((prev) => prev.filter((set) => parseInt(set.props.id) !== id));
   }
-  function handleEdit():void {
-    setSetsDisabled(false)
+  function handleEdit(): void {
+    setSetsDisabled(false);
   }
 
   useEffect(() => {
-    setSets(setsArray)
-  },[setsDisabled])
+    setSets(setsArray);
+  }, [setsDisabled]);
 
-useEffect(() => {
-    setSets(setsArray)
-}, [])
+  useEffect(() => {
+    setSets(setsArray);
+  }, []);
 
   return (
     <div className="pages-Div">
       <h2>Workout Summary</h2>
-
       <Form id="summaryForm" onFinish={onFinish}>
-        <Form.Item name="workout_name" label="Give your workout a name" rules={[
+        <Form.Item
+          name="workout_name"
+          label="Give your workout a name"
+          rules={[
             {
               required: true
             }
           ]}>
-        <Input placeholder='e.g. Workout 1' />
+          <Input placeholder="e.g. Workout 1" />
         </Form.Item>
-        <p>Would you like to edit your sets? <Button onClick={()=>handleEdit()}>Edit</Button></p>
+        <p>
+          Would you like to edit your sets? <Button onClick={() => handleEdit()}>Edit</Button>
+        </p>
         {sets}
-        <Button type="primary" htmlType="submit" >Save workout</Button>
-        </Form>;
-
-    </div>)
-}
+        <Button type="primary" htmlType="submit">
+          Save workout
+        </Button>
+      </Form>
+      ;
+    </div>
+  );
+};
 
 export default WorkoutSummary;
