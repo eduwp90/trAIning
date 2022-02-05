91121e89c6811f6162a7e57ab3075e8e8321617a
Jaime D. Rguez-Mastretta <jaimemastretta@Jaimes-MacBook-Pro.local>
2022-02-05 14:08:55 +0100

    feat: default workouts added with new props

--- client/src/interfaces.ts
+++ client/src/interfaces.ts
@@ -9,13 +9,15 @@ export interface ISet {
 export interface IWorkout {
   user: string;
   name: string;
+  time: number;
+  calories: number;
   workout: ISet[];
 }
 
-export interface IWorkoutContext{
-  workout: ISet[],
-  storeWorkout: (sets: ISet[]) => void,
-  clearWorkout: ()=> void
+export interface IWorkoutContext {
+  workout: ISet[];
+  storeWorkout: (sets: ISet[]) => void;
+  clearWorkout: () => void;
 }
 export interface IWorkoutResponse extends IWorkout, DocumentData {
   id: string;
